895ca4758acaee5ac76b0bcb993a149e33a2be2c
egulliver1721 <118499423+egulliver1721@users.noreply.github.com>
2023-01-06 18:04:53 +0800

    set up data and basic HTML CSS

---
+++ tagData.js
@@ -0,0 +1,110 @@
+export const tagData = [
+  {
+    tagItems: "nappies",
+    category: "changing",
+  },
+  {
+    tagItems: "wipes",
+    category: "changing",
+  },
+  {
+    tagItems: "change mat",
+    category: "changing",
+  },
+  {
+    tagItems: "nappy bags",
+    category: "changing",
+  },
+  {
+    tagItems: "nappy cream",
+    category: "changing",
+  },
+  {
+    tagItems: "sanitiser",
+    category: "changing",
+  },
+  {
+    tagItems: "water",
+    category: "feeding",
+  },
+  {
+    tagItems: "formula",
+    category: "feeding",
+  },
+  {
+    tagItems: "bottle",
+    category: "feeding",
+  },
+  {
+    tagItems: "snacks",
+    category: "feeding",
+  },
+  {
+    tagItems: "bib",
+    category: "feeding",
+  },
+  {
+    tagItems: "cloth",
+    category: "feeding",
+  },
+  {
+    tagItems: "keys",
+    category: "parent",
+  },
+  {
+    tagItems: "phone",
+    category: "parent",
+  },
+  {
+    tagItems: "wallet",
+    category: "parent",
+  },
+  {
+    tagItems: "chapstick",
+    category: "parent",
+  },
+  {
+    tagItems: "sunglasses",
+    category: "parent",
+  },
+  {
+    tagItems: "hat",
+    category: "parent",
+  },
+  {
+    tagItems: "sunscreen",
+    category: "clothing",
+  },
+  {
+    tagItems: "wrap",
+    category: "clothing",
+  },
+  {
+    tagItems: "blanket",
+    category: "clothing",
+  },
+  {
+    tagItems: "spare clothes",
+    category: "clothing",
+  },
+  {
+    tagItems: "dummy",
+    category: "soothing/play",
+  },
+  {
+    tagItems: "teether",
+    category: "soothing/play",
+  },
+  {
+    tagItems: "toy",
+    category: "soothing/play",
+  },
+  {
+    tagItems: "first aid",
+    category: "other",
+  },
+  {
+    tagItems: "nail file",
+    category: "other",
+  },
+];
